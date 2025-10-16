49524f45508d4689690e91493d5405d78e479338
Yosmany Garcia <yosmany.garcia@eleris.com>
2025-10-16 14:17:21 -0700

    Implemented a basic form to test address parse

---
+++ src/index.ts
@@ -0,0 +1,48 @@
+import {RestApplication, post, requestBody} from '@loopback/rest';
+import * as path from 'path';
+import postal from 'node-postal';
+import {addressParse} from '@zerodep/address';
+
+class ApiController {
+  @post('/api')
+  async submit(
+    @requestBody() body: {address: string, lib: string}
+  ): Promise<any> {
+    const lib = body.lib || 'node-postal';
+
+    if (lib === 'node-postal') {
+      return parseWithNodePostal(body.address);
+    } else if (lib === '@zerodep/address') {
+      return parseWithZerodep(body.address);
+    } else {
+      throw new Error('Unknown library');
+    }
+  }
+}
+
+const parseWithNodePostal = (address: string) => {
+  const result = postal.parser.parse_address(address);
+  const formatted: Record<string, string> = {};
+
+  result.forEach(({component, value}) => {
+    formatted[component] = value;
+  });
+
+  return formatted;
+}
+
+const parseWithZerodep = (address: string) => {
+  return addressParse(address);
+}
+
+export async function main(options: object = {}) {
+  const app = new RestApplication(options);
+  app.controller(ApiController);
+  app.static('/', path.join(__dirname, '../public'));
+  await app.start();
+  console.log(`Server is running at http://localhost:3000`);
+}
+
+if (require.main === module) {
+  main();
+}
